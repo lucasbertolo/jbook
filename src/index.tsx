d4dc376469dcbb9f976502f40e0c0d2b5a16839a
lucasbertolo <lucasbertolo2@gmail.com>
2021-04-23 16:28:03 -0300

    Creating actions and store for redux files

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   );
 };
 
