9ca3032b44f9cb51a748ec8e2ad000d0bd525a39
lucasbertolo <lucasbertolo2@gmail.com>
2021-05-09 13:02:36 -0300

    Styling add cell divider

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -1,26 +1,37 @@
 import './add-cell.css';
 import { useActions } from '../hooks/use-actions';
+import { FaPlus } from 'react-icons/fa';
 interface AddCellProps {
-  nextCellId: string;
+  nextCellId: string | null;
+  forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div>
+    <div className={`add-cell ${forceVisible && 'force-visibility'}`}>
       <button
+        className="button is-rounded is-small is-primary"
         onClick={() => insertCellBefore(nextCellId, 'code')}
         type="button"
       >
-        Code
+        <span className="icon">
+          <FaPlus />
+        </span>
+        <span>Code</span>
       </button>
       <button
+        className="button is-rounded is-small is-primary"
         onClick={() => insertCellBefore(nextCellId, 'text')}
         type="button"
       >
-        Text
+        <span className="icon">
+          <FaPlus />
+        </span>
+        <span>Text</span>
       </button>
+      <div className="divider" />
     </div>
   );
 };
--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 
 const CellList: React.FC = () => {
@@ -7,10 +9,18 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} forceVisible={cells.length === 0} />
+    </div>
+  );
 };
 
 export default CellList;
